8fa2d773e1ed9ed938b85f0365db2d92098d18a6
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-08-11 12:44:35 +0300

    hello world

---
+++ ksp-graphics-overhaul/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace ksp_graphics_overhaul
+{
+
+    public class Class1 : PartModule
+    {
+        public override void OnStart(StartState state)
+        {
+            print("Hello world!");
+        }
+    }
+
+}
